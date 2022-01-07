8575d734b71b0338fa52bd6f17209f1953f7d2c9
jh0222 <gihyun0222@naver.com>
2022-01-07 18:46:43 +0900

    [UPDATE]dev db commit

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -47,22 +47,35 @@ insert into boss_table values(boss_seq.nextval,'김연아','kim2','k12345!','kim
 select * from boss_TABLE;
 
 3. 캠핑 테이블
+/*
+ * db에서 가져올때 cam_no=cam_no -> update cam_number-headcont
+*/
+drop table camping_table;
 create table camping_table(
 	cam_no number(5) primary key,
-	cam_bo_no number(5) not null,
+	cam_bo_id varchar2(20 char) not null,
 	cam_name varchar2(30 char) not null,
 	cam_txt varchar2(200 char) not null,
 	cam_phonenumber varchar2(30 char) not null,
 	cam_price number(10) not null,
-	cam_address address varchar2(100 char) not null,
-	cam_headcount number(5) not null
-)
+	cam_address varchar2(100 char) not null,
+	cam_latitude varchar2(30 char) not null,	/*위도*/
+	cam_longitude varchar2(30 char) not null,	/*경도*/
+	cam_headcount number(5) not null	
+);
 
 create sequence camping_seq;
 
-insert into camping_table values('1',camping_seq.nextval,'난지캠핑장')
+insert into camping_table values(camping_seq.nextval,'kim2','난지캠핑장','캠핑장','02-373-2021',20000,'서울 마포구 한강난지로 28','37.57035','126.87264',40);
+
+select * from camping_table;
 
 4. 예약 테이블
+/*
+startdate~enddate예약되어있으면 처음부터 막아놓기,어제 날짜 막아놓기			
+dbex) where sysdate> and startdate< enddate>  where cam_number			
+*/
+
 create table reservation_table(
 	r_no number(5) primary key,
 	r_cam_no number(5) not null,
@@ -76,5 +89,317 @@ create table reservation_table(
 	r_campingprice number(10) not null,
 	r_cam_phonenumber varchar2(30 char) not null,
 	r_cam_address varchar2(100 char) not null
-)
+);
+
+create sequence reservation_seq;
+
+insert into reservation_table values(reservation_seq.nextval,1,'kim','김태희','01012341234','난지캠핑장','20220318','20220320',2,40000,'02-373-2021','서울 마포구 한강난지로 28');
+
+select * from reservation_table;
+
+5. 캠핑찜
+drop table campingheart_table;
+create table campingheart_table(
+	h_no number(5) primary key,
+	h_cam_no number(5) not null,
+	h_u_id varchar2(20 char) not null,
+	h_cam_name varchar2(30 char) not null,
+	h_cam_address varchar2(100 char) not null,
+	h_campingheart number(5) not null
+);
+
+create sequence campingheart_seq;
+
+insert into campingheart_table values(campingheart_seq.nextval,1,'kim','난지캠핑장','서울 마포구 한강난지로 28',1);
+
+select * from campingheart_table;
+
+6. 캠핑 리뷰
+/*
+예약하고 그 날짜가 지나면 리뷰적기 버튼으로 바뀜
+ex)enddate=sysdate+1
+*/
+drop table campingreview_table;
+create table campingreview_table(		
+	c_no number(5) primary key,
+	c_cam_no number(5) not null,
+	c_u_id varchar2(20 char) not null,
+	c_r_campingenddate date not null,
+	c_campingreview	varchar2(100) not null,
+	c_campingstar number(5) not null,
+	c_date date not null
+);
+
+create sequence campingreview_seq;
+
+insert into campingreview_table values(campingreview_seq.nextval,1,'kim','20220320','좋아요',5,'20220321');
+
+select * from campingreview_table;
+
+7. 캠핑 리뷰 댓글달기(사장이)
+/*
+ * 사장이 자신의 캠핑에서만 대댓글 달게 만들기
+ */
+
+create table campingreview_bossreply_table(	
+	cr_no number(5) primary key,
+	cr_cam_no number(5) not null,
+	cr_bo_id varchar2(20 char) not null,
+	cr_u_id	varchar2(20 char) not null,
+	cr_replytxt	varchar2(100 char) not null,
+	cr_date	date not null
+);
+
+create sequence campingreview_bossreply_seq;
+
+insert into campingreview_bossreply_table values(campingreview_bossreply_seq.nextval,1,'kim2','kim','감사합니다','20220322');
+
+select * from campingreview_bossreply_table;
+
+8. 캠핑용품 등록(관리자만)
+create table product_registration_table(
+	p_no number(5) primary key,
+	p_name varchar2(20 char) not null,
+	p_price	number(10) not null,
+	p_picture varchar2(200 char) not null,
+	p_txt varchar2(200 char) not null
+);
+
+create sequence product_registration_seq;
+
+insert into product_registration_table values(product_registration_seq.nextval,'캠핑용품',10000,'b.jpg','캠핑용품입니다');
+
+select * from product_registration_table;
+
+9. 밀키트 등록(관리자만)
+create table foodproduct_registration_table(
+	fp_no number(5) primary key,
+	fp_name varchar2(20 char) not null,
+	fp_price	number(10) not null,
+	fp_picture varchar2(200 char) not null,
+	fp_txt varchar2(200 char) not null
+);
+
+create sequence foodproduct_registration_seq;
+
+insert into foodproduct_registration_table values(foodproduct_registration_seq.nextval,'캠핑용품',10000,'b.jpg','캠핑용품입니다');
+
+select * from foodproduct_registration_table;
+
+10. 캠핑용품 등록(관리자만)
+/*
+ * 하나만 선택되도록 radio box
+ * 새로운 배송지가 null값이면 기존배송지 아니면 새로운 배송지가 뜨도록 만들기
+ * */
+drop table guest_product_buy_table;
+create table guest_product_buy_table(		
+	b_no number(5) primary key, /*(주문번호)*/
+	b_u_bo_id varchar2(20 char) not null,
+	b_p_no number(5) not null,
+	b_p_name varchar2(20 char) not null,
+	b_price	number(5) not null, /*(수량*p_price)*/
+	b_number number(5) not null,
+	b_u_address varchar2(100 char) not null, /*(기존 자기 정보 배송지)*/
+	b_new_address varchar2(100 char) null,
+	b_date date not null
+);
+
+create sequence g_p_buy_seq;
+
+insert into guest_product_buy_table values(g_p_buy_seq.nextval,'kim',1,'캠핑용품',10000,1,'서울특별시 종로구',null,'20220306');
+
+select * from guest_product_buy_table;
+
+11. 밀키트 등록(관리자만)
+/*
+ * 하나만 선택되도록 radio box
+ * 새로운 배송지가 null값이면 기존배송지 아니면 새로운 배송지가 뜨도록 만들기
+ * */
+drop table guest_product_buy_table;
+create table guest_foodproduct_buy_table(		
+	fb_no number(5) primary key, /*(주문번호)*/
+	fb_u_bo_id varchar2(20 char) not null,
+	fb_p_no number(5) not null,
+	fb_p_name varchar2(20 char) not null,
+	fb_price	number(5) not null, /*(수량*p_price)*/
+	fb_number number(5) not null,
+	fb_u_address varchar2(100 char) not null, /*(기존 자기 정보 배송지)*/
+	fb_new_address varchar2(100 char) null,
+	fb_date date not null
+);
+
+create sequence g_fp_buy_seq;
+
+insert into guest_foodproduct_buy_table values(g_fp_buy_seq.nextval,'kim',1,'캠핑용품',10000,1,'서울특별시 종로구',null,'20220306');
+
+select * from guest_foodproduct_buy_table;
+
+12. 캠핑용품 장바구니
+create table guest_product_basket_table(
+	ba_no number(5) primary key,
+	ba_p_no	number(5) not null,
+	ba_u_bo_id varchar2(20 char) not null,
+	ba_p_name varchar2(20 char) not null,
+	ba_price number(5) not null, /*(수량*p_price)*/
+	ba_number number(5) not null
+);
+
+create sequence product_basket_seq;
+
+insert into guest_product_basket_table values(product_basket_seq.nextval,1,'kim','캠핑용품',10000,1);
+
+select * from guest_product_basket_table;
+
+13. 밀키트 장바구니
+create table guest_foodproduct_basket_table(
+	fba_no number(5) primary key,
+	fba_fp_no	number(5) not null,
+	fba_u_bo_id varchar2(20 char) not null,
+	fba_fp_name varchar2(20 char) not null,
+	fba_price number(5) not null, /*(수량*p_price)*/
+	fba_number number(5) not null
+);
+
+create sequence foodproduct_basket_seq;
+
+insert into guest_foodproduct_basket_table values(foodproduct_basket_seq.nextval,1,'kim','캠핑용품',10000,1);
+
+select * from guest_foodproduct_basket_table;
+
+14. 캠핑용품 리뷰
+create table product_review_table(	
+	pr_no number(5)	primary key,
+	pr_u_bo_id varchar2(20 char) not null,
+	pr_txt varchar2(100 char) not null,
+	pr_date	date not null
+);
+
+create sequence product_review_seq;
+
+insert into product_review_table values(product_review_seq.nextval,'kim','좋아요','20220301');
+
+select * from product_review_table;
+
+15. 밀키트 리뷰
+create table foodproduct_review_table(	
+	fpr_no number(5) primary key,
+	fpr_u_bo_id varchar2(20 char) not null,
+	fpr_txt varchar2(100 char) not null,
+	fpr_date date not null
+);
+
+create sequence foodproduct_review_seq;
+
+insert into foodproduct_review_table values(foodproduct_review_seq.nextval,'kim','좋아요','20220301');
+
+select * from foodproduct_review_table;
+
+16. 자유게시판
+create table free_board_table(	
+	f_no number(5) primary key,
+	f_u_id varchar2(20 char) not null,
+	f_subject varchar2(20 char)	not null,
+	f_txt varchar2(100 char) not null,
+	f_picture varchar2(200 char) null,
+	f_readcount	number(5) not null,
+	f_date date	not null
+);
+
+create sequence free_board_seq;
+
+insert into free_board_table values(free_board_seq.nextval,'kim','자유게시판','자유자유자유','h.jpg',0,'20220110');
+
+select * from free_board_table;
+
+17. 자유게시판 리뷰
+drop table free_boad_reply_table
+create table free_board_reply_table(
+	fr_no number(5) primary key,
+	fr_f_no	number(5) not null,
+	fr_u_id	varchar2(100 char) not null,
+	fr_owner_no	varchar2(100 char) null,
+	fr_owner_id	varchar2(100 char) null,
+	fr_replytxt	varchar2(100 char) not null,
+	fr_date	date not null
+);
+
+create sequence free_board_reply_seq;
+
+insert into free_board_reply_table values(free_board_reply_seq.nextval,1,'kim',null,null,'댓글입니다','20220210');
+
+select * from free_board_reply_table;
+
+18. 캠핑팁게시판
+create table campingtip_board_table(	
+	tip_no number(5) primary key,
+	tip_u_id varchar2(20 char) not null,
+	tip_subject varchar2(20 char)	not null,
+	tip_txt varchar2(100 char) not null,
+	tip_picture varchar2(200 char) null,
+	tip_readcount	number(5) not null,
+	tip_date date	not null
+);
+
+create sequence campingtip_board_seq;
+
+insert into campingtip_board_table values(campingtip_board_seq.nextval,'kim','자유게시판','자유자유자유','h.jpg',0,'20220110');
+
+select * from campingtip_board_table;
+
+19. 캠핑팁 리뷰
+create table campingtip_board_reply_table(
+	tipr_no number(5) primary key,
+	tipr_f_no	number(5) not null,
+	tipr_u_id	varchar2(100 char) not null,
+	tipr_owner_no	varchar2(100 char) null,
+	tipr_owner_id	varchar2(100 char) null,
+	tipr_replytxt	varchar2(100 char) not null,
+	tipr_date	date not null
+);
+
+create sequence campingtip_board_reply_seq;
+
+insert into campingtip_board_reply_table values(campingtip_board_reply_seq.nextval,1,'kim',null,null,'댓글입니다','20220210');
+
+select * from campingtip_board_reply_table;
+
+20. 레시피 게시판
+create table recipe_board_table(	
+	rb_no number(5) primary key,
+	rb_u_id varchar2(20 char) not null,
+	rb_subject varchar2(20 char)	not null,
+	rb_txt varchar2(100 char) not null,
+	rb_picture varchar2(200 char) null,
+	rb_readcount	number(5) not null,
+	rb_date date	not null
+);
+
+create sequence recipe_board_seq;
+
+insert into recipe_board_table values(recipe_board_seq.nextval,'kim','자유게시판','자유자유자유','h.jpg',0,'20220110');
+
+select * from recipe_board_table;
+
+21. 레시피 리뷰
+create table recipe_board_reply_table(
+	rr_no number(5) primary key,
+	rr_f_no	number(5) not null,
+	rr_u_id	varchar2(100 char) not null,
+	rr_owner_no	varchar2(100 char) null,
+	rr_owner_id	varchar2(100 char) null,
+	rr_replytxt	varchar2(100 char) not null,
+	rr_date	date not null
+);
+
+create sequence recipe_board_reply_seq;
+
+insert into recipe_board_reply_table values(recipe_board_reply_seq.nextval,1,'kim',null,null,'댓글입니다','20220210');
+
+select * from recipe_board_reply_table;
+
+
+
+
+
+
 
